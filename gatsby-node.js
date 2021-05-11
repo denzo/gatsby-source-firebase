bb7ca15a301a3043651c23cf33dd20c6c58557d3
Deniss Alimovs <deniss.alimovs@gmail.com>
2021-05-11 17:55:14 +1000

    async

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,65 +1,57 @@
-const firebase = require("firebase-admin")
-const crypto = require("crypto")
 
-exports.sourceNodes = (
+const report = require('gatsby-cli/lib/reporter');
+const firebase = require('firebase-admin');
+const crypto = require('crypto');
+
+const getDigest = id =>
+  crypto
+    .createHash('md5')
+    .update(id)
+    .digest('hex');
+
+exports.sourceNodes = async (
   { actions },
-  { credential, databaseURL, types, quiet = false },
-  done
+  { types, credential, appConfig }
 ) => {
-  const { createNode } = actions
-
-  firebase.initializeApp({
-    credential: firebase.credential.cert(credential),
-    databaseURL: databaseURL
-  })
-
-  const db = firebase.firestore()
-  db.settings({ timestampsInSnapshots: true })
-
-  const start = Date.now()
-
-  types.forEach(
-    ({ query = ref => ref, map = node => node, type, path }) => {
-      if (!quiet) {
-        console.log(`\n[Firebase Source] Fetching data for ${type}...`)
-      }
-
-      firebase.firestore().collection(`${path}`).get().then(querySnapshot => {
-        if (!quiet) {
-          console.log(
-            `\n[Firebase Source] Data for ${type} loaded in`,
-            Date.now() - start,
-            "ms"
-          )
-        }
-
-        const val = querySnapshot
-
-        Object.keys(val).forEach(key => {
-          const node = map(Object.assign({}, val[key]))
-
-          const contentDigest = crypto
-            .createHash(`md5`)
-            .update(JSON.stringify(node))
-            .digest(`hex`)
-
-          createNode(
-            Object.assign(node, {
-              id: key,
-              parent: "root",
-              children: [],
-              internal: {
-                type: type,
-                contentDigest: contentDigest
-              }
-            })
-          )
-        })
-        done()
-      })
-    },
-    error => {
-      throw new Error(error)
+  try {
+    if (firebase.apps || !firebase.apps.length) {
+      const cfg = appConfig ? appConfig : {credential: firebase.credential.cert(credential)}
+      firebase.initializeApp(cfg);
     }
-  )
-}
+  } catch (e) {
+    report.warn(
+      'Could not initialize Firebase. Please check `credential` property in gatsby-config.js'
+    );
+    report.warn(e);
+    return;
+  }
+  const db = firebase.firestore();
+  db.settings({
+    timestampsInSnapshots: true,
+  });
+
+  const { createNode } = actions;
+
+  const promises = types.map(
+    async ({ collection, type, map = node => node }) => {
+      const snapshot = await db.collection(collection).get();
+      for (let doc of snapshot.docs) {
+        const contentDigest = getDigest(doc.id);
+        createNode(
+          Object.assign({}, map(doc.data()), {
+            id: doc.id,
+            parent: null,
+            children: [],
+            internal: {
+              type,
+              contentDigest,
+            },
+          })
+        );
+        Promise.resolve();
+      }
+    }
+  );
+  await Promise.all(promises);
+  return;
+};
